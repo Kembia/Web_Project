5386925901e4f436c8edfb39777895518845919d
Kembia <64161831+Kembia@users.noreply.github.com>
2024-08-24 16:13:14 +0200

    Cities

---
+++ cities_Page/script.js
@@ -0,0 +1,23 @@
+function openCity(evt, cityName) {
+  
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+      
+  // Hide all elements with class="tabcontent" by default */
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+  
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
